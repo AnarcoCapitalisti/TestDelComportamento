6f2709db81a59d06b1d1b6184889210e1660fba2
pippo <pippo@pippo-pc.homegroup>
2023-02-21 18:41:48 +0100

    Commit dello script

---
+++ main.py
@@ -0,0 +1,153 @@
+domande = {
+    "Evitante": [
+        "Ha mai evitato lavori o incarichi per cui era necessario avere a che fare con molte persone?",
+        "Ha mai evitato di instaurare rapporti con persone a cui non era sicuro di piacere?",
+        "Ha difficolta' ad 'aprirsi' anche con le persone a cui e' piu' legato?",
+        "E' spesso preoccupato, quando incontra altre persone, di eventuali critiche o di essere rifiutato?",
+        "E' solitamente silenzioso o timido quando incontra persone nuove?",
+        "Ha paura di provare nuove esperienze, di fare cose nuove?",
+    ],
+    "Dipendente": [
+        "Ha bisogno di molti consigli e rassicurazioni dagli altri prima di prendere anche piccole decisioni (ad esempio, cosa indossare o cosa ordinare al ristorante)?",
+        "Preferisce che siano altre persone a gestire oa  prendere decisioni su importanti aspetti della Sua vita, come la gestione delle finanze, l'educazione dei figli o dove andare ad abitare?",
+        "Ha difficolta' ad esprimere il Suo disaccordo, anche se pensa che gli altri abbiano torto?",
+        "E' difficile per Lei iniziare delle attivita' o seguire dei compiti senza l'aiuto di qualcuno?",
+        "Si e' prestato spesso per svolgere compiti poco sgradevoli?",
+        "Di solito si sente a disagio quando e' da solo?",
+        "Quando finisce una relazione importante, avverte subito il bisogno di trovare un'altra persona che si prenda cura di Lei?",
+        "Ha molta paura di essere lasciato solo senza nessuno che si prenda cura di Lei?"
+    ],
+    "Ossessivo compulsivo": [
+        "Lei e' una persona che presta molta attenzione ai dettagli, all'ordine, all'organizzazione e che ama fare elenchi, schemi o piani?",
+        "Ha difficolta' a finire i lavori, perche' impiega troppo tempo cercando di fare ogni cosa alla perfezione?",
+        "Pensa o altri pensano che Lei si dedica cosi' tanto al lavoro (o alla scuola) da non aver tempo per nient'altro o anche solo per divertirsi?",
+        "Ha delle convinzioni molto rigide e rigorose rispetto a cio' che e' giusto o sbagliato, che si deve non si deve fare?",
+        "Fa fatica a buttar via le cose, perche' pensa che un giorno Le potrebbero servire?",
+        "E' difficile per Lei accettare di essere aiutato da altre persone a meno che non accettino di fare le cose esattamente come vuole Lei?",
+        "Spende con difficolta' denaro per Lei o per altre persone anche quando se lo potrebbe permettere?",
+        "Lei e' spesso cosi' sicuro di avere ragione da non prendere in considerazione piu' di tanto cio' che pensano gli altri?",
+        "E' mai stato definito da altre persone come rigido o testardo?"
+    ],
+    "Passivo Aggressivo": [
+        "Quando qualcuno Le chiede di fare qualcosa, che Lei non vuole fare o che non Le piace, accetta, ma poi Le capita di fare quella cosa male o lentamente?",
+        "Se non vuole o non Le piace fare una cosa, spesso la 'dimentica'?",
+        "Ha spesso l'impressione che gli altri non La capiscano o che non apprezzino abbastanza cio' che fa?",
+        "E' spesso irritabile e Le capita di litigare con una certa facilita'?",
+        "Ha mai pensato che la maggior parte dei Suoi capi, insegnanti, superiori, medici e altri professionisti, che dovrebbero conoscere bene il proprio mestiere, di fatto spesso non lo conoscono?",
+        "Le capita spesso che, dopo essersi rifiutato di fare qualcsoa perche' e' arrabbiato, piu' tardi si sente in colpa e si scusa?",
+    ],
+    "Depressivo": [
+        "Si sente spesso infelice e pensa che la vita non sia bella?",
+        "Si sente una persona fondamentalmente non all'altezza delle situazioni e di non essere contento di se stesso?",
+        "Tende spesso a giudicarsi negativamente?",
+        "Continua a ripensare a vicende negative del passato o si preoccupa di quelle che potrebbero verificarsi in futuro?",
+        "Giudica spesso gli altri con asprezza e trova che hanno dei difetti e delle colpe?",
+        "Si aspetta quasi sempre che le cose vadano a finire male?",
+        "Si sente spesso in colpa per avere o non avere fatto determinate cose?",
+    ],
+    "Paranoide": [
+        "Le capita spesso di dover stare attento per evitare che gli altri possano approfittare di Lei per sfruttarLa o ferirLa?",
+        "Passa molto tempo a chiedersi se puo' fidarsi dei Suoi amici o delle persone con cui lavora?",
+        "Ritiene che sia meglio non sapere molte cose sul Suo conto perche' gli altri potrebbero approfittarne e usarle contro di Lei?",
+        "Le capita spesso di vedere che dietro a cio' che la gente fa o dice vi sono come delle minacce velate o delle offese?",
+        "Lei e' piu' il tipo di persona che serba rancore e impiega molto tempo prima di perdonare le persone che L'hanno offesa e Le hanno mancato di rispetto?",
+        "Sono molte le persone alle quali non riesce a perdonare per qualcsoa che hanno fatto o detto contro di Lei molto tempo fa?",
+        "Si arrabbia facilmente, o diventa aggressivo, quando qualcuno La critica o La offende in qualche modo?",
+        "Le capita spesso di sospettare che il Suo coninuge o il Suo compagno Le sia infedele?"
+    ],
+    "Schizotipico": [
+        "Quando si trova in pubblico e vede la gente converersare, ha spesso la sensazione che stiano parlando di Lei?",
+        "Ha spesso la sensazione che la maggior parte delle persone abbiano invece un messaggio speciale rivolto a Lei?",
+        "Quando si trova tra la gente, ha spesso la sensazione di essere ossservato o fissato in modo particolare?",
+        "Si e' mai sentito in grado di fare accadere alcune cose soltanto esprimendo un desiderio o pensandovi?",
+        "Ha mai avuto esperience personali di fenomeni paranormali?",
+        "Crede di aver un certo 'sesto senso' grazie al quale e' in grado di conoscere o prevedere cio' che gli altri ignorano?",
+        "Le sembra spesso di scorgere persone o animali negli oggeti o nelle ombre, oppure di percepire i rumori come voci?",
+        "Ha mai avvertito la presenza di persone o di una forza attorno a se', pur non vedendo nulla?",
+        "Vede spesso aure o campi di energia attorno alle persone?",
+    ],
+    "Schizoide": [
+        "Sono poche le persone alle quali e' legato al di fuori dei Suoi piu' stretti familiari?",
+        "Si sente spesso nervoso quando e' in compagnia di altre persone?",
+        "Per Lei NON e' importante avere o meno relazioni strette con altre persone?",
+        "Preferisce quasi sempre fare le cose da solo piuttosto che assieme ad altre persone?",
+        "Puo' sentirsi soddisfatto anche senza essere coinvolto in relazioni di tipo sessuale con altre persone?",
+        "Sono veramente poche le cose che Le danno piacere?",
+        "NON Le importa piu' di tanto cio' che la gente pensa di Lei?",
+        "Non esiste niente in grado di renderLa particolarmente felice o particolarmente triste?",
+    ],
+    "Istrionico": [
+        "Le piace trovarsi al centro dell'attenzione?",
+        "Le piace molto essere seducente?",
+        "Si fa spesso avanti per primo con le persone che Le piacciono?",
+        "Cerca di attirare l'attenzione su di se' curando il Suo aspetto e il Suo abbigliamento?",
+        "Ritiene importante esprimersi in modo vivace e pittoresco quando incontra altre persone?",
+        "Cambia spesso opinione a seconda delle persone che frequenta o in base a cio' che ha appena appreso dai giornali o dalla televisione?",
+        "Ha molti amici a cui e' strettamente legato?"
+    ],
+    "Narcisistico": [
+        "Spesso la gente non apprezza le Sue doti elevate e le Sue qualita' speciali?",
+        "Altre persone Le hanno fatto notare che ha un'opinione troppo elevata di se stesso?",
+        "Pensa molto al potere, alla fama e ai riconoscimenti che un giorno o l'altro raggiungera'?",
+        "Immagina spesso la perfetta storia d'amore che un giorno Le accadra' di certo?",
+        "Quando ha un problema, insiste quasi sempre per parlarne con la persona piu' importante?",
+        "Ritiene sia molto importante trascorrere il proprio tempo con persone speciali o influenti?",
+        "Per Lei e' molto importante ricevere in qualche modo l'attenzione e l'ammirazione di altre persone?",
+        "Non ritiene necessario seguire determinate regole o convenzioni sociali quando Le sono di impiccio e potrebbero ostacolarLa?",
+        "Sente di essere il tipo di persona ch esi merita un trattamento speciale?",
+        "Ritiene spesso necessario pestare i piedi a qualcuno per ottenere cio' che vuole?",
+        "Mette i Suoi bisogni, le Sue priorita' al di sopra delle esigenze degli altri?",
+        "Si aspetta che gli altri facciano quello che chiede senza troppe domande anche solo per quello che Lei rappresenta?",
+        "NON sente di essere molto interessato ai problemi o ai sentimenti delle altre persone?",
+        "Gli altri si lamentano mai del fatto che Lei non li ascolta e che non e' attento ai loro sentimenti?",
+        "Prova spesso invidia verso le altre persone?",
+        "Ha spesso l'impressione che gli altri siano invidiosi di Lei?",
+        "Ritiene ben poche persone meritevoli del Suo tempo e della Sua attenzione?"
+    ],
+    "Borderline": [
+        "Si e' spesso sentito molto agitato, pensando che una persona a cui teneva veramente fosse sul punto di lasciarLa?",
+        "Le elazioni con persone a cui tiene veramente sono caratterizzate da frequenti e intensi alti e bassi?",
+        "Ha mai cambiato in modo improvviso l'idea che aveva di se' e dei Suoi obbiettivi?",
+        "L'idea che ha di se stesso cambia spesso in modo notevole?",
+        "Lei e' diverso a seconda delle persone o delle situazioni che incontra tanto che a volte stenta a riconoscere chi Lei sia veramente?",
+        "Ha cambiato spesso e improvvisamente i Suoi obiettivi, i piano per la Sua carriera, le Sue credenze religiose, ecc.?",
+        "Si e' trovato spesso ad agire d'impulso?",
+        "Ha mai tentato o minacciato di ferirsi, di farsi del male o di uccidersi?",
+        "Si e' mai tagliato, bruciato o graffiato di proposito?",
+        "Cambia umore spesso e improvvisamente?",
+        "Avverte spesso un senso di vuoto dentro?",
+        "Ha spesso un temperamento irruente e si arrabbia cose' tanto da perdere il controllo?",
+        "Quando e' in collera, Le capita di picchiare le persone o lanciare oggetti?",
+        "Spesso anche delle piccole cose La fanno arrabbiare?",
+        "In momenti di stress elevato, quando e' molto teso, diventa sospettoso oppure si sente particolarmente strano, 'fuori', confuso?",
+    ],
+    "Antisociale": [
+        "Prima dei 15 anni, faceva spesso il prepotente o intimoriva gli altri bambini o ragazzi?",
+        "Prima dei 15 anni, Le e' capitato di dare inizio a sontri e liti con altri bambini?",
+        "Prima dei 15 anni, ha ferito o minacciato qualcuno con un'arma, ad esempio con un bastone, una pietra, una bottiglia rotta, un coltello o una pistola?",
+        "Prima dei 15 anni, ha torturato o ferito animali di proposito?",
+        "Prima dei 15 anni, ha derubato, rapinato o preso con la forza qualcosa minacciando direttamente altre persone?",
+        "Prima dei 15 anni, ha forzato qualcuno ad avere rapporti sessuali, a spogliarsi davanti a Lei o a toccarLa?",
+        "Prima dei 15 anni, distruggeva deliberatamente cose che non Le appartenevano?",
+        "Prima dei 15 anni, entrava abusivamente in abitazioni, edifici o macchine di altri?",
+        "Prima dei 15 anni, mentiva spesso o 'faceva fessi' gli altri?",
+        "Prima dei 15 anni, spesso rubava, prendeva cose dai negozi o falsificava la firma di altre persone?",
+        "Prima dei 15 anni, e' fuggito da casa trascorrendo la notte fuori?",
+        "Prima dei 13 anni, rimaneva spesso fuori di notte, rientrando molto piu' tardi di quanto avrebbe dovuto?",
+        "Prima dei 13 anni, salvava spesso la scuola?"
+    ]
+}
+punti_totali={}
+for tratto in domande:
+    punti=0
+    for domanda in domande[tratto]:
+        while 1:
+            answ=input(domanda+" (s/n) ").lower()[:1]
+            if answ=="s":
+                punti+=1
+                break
+            elif answ=="n":
+                break
+    punti_totali[tratto]=punti
+for tratto in punti_totali:
+    print(tratto, (punti_totali[tratto]/len(domande[tratto]))*100)
